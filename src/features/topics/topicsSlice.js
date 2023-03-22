203bbd8e94075781c01c1cc2a03244b5ed043935
Alvaro-LGPA <alvarolaura@gmail.com>
2023-03-22 11:28:02 +0100

    Refactor for clarity

--- src/features/topics/topicsSlice.js
+++ src/features/topics/topicsSlice.js
@@ -1,6 +1,5 @@
 import { createSlice} from "@reduxjs/toolkit";
 
-
 const topicsSlice  = createSlice(
     {
         name: "topics",
@@ -9,17 +8,26 @@ const topicsSlice  = createSlice(
         },
         reducers: {
             addTopic: (state, action) => {
-                
-                const quizIds = [];
-                state.topics[action.payload.id] = {...action.payload,quizIds};
+                const {id, name, icon} = action.payload;
+    
+                //const quizIds = [];
+                // state.topics[id] = {...action.payload,quizIds};
+                state.topics[id] = {
+                    id: id,
+                    name: name,
+                    icon: icon,
+                    quizIds: []
+                }
             },
             addQuizId: (state, action) => {
-                state.topics[action.payload.topicId].push(action.payload.quizId)
+                const {topicId, quizId} = action.payload;
+                state.topics[topicId].quizIds.push(quizId);
             }
         }
 
     }
 )
+
 
 export const { addTopic, addQuizId } = topicsSlice.actions;
 export const selectTopics = (state) => state.topics.topics;
