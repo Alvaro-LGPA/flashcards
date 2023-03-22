6b5e0ed829fe894ea71167afe596983e7a69d927
Alvaro-LGPA <alvarolaura@gmail.com>
2023-03-22 11:27:43 +0100

    Fix thunk

--- src/features/quizzes/quizzesSlice.js
+++ src/features/quizzes/quizzesSlice.js
@@ -4,27 +4,34 @@ import { addQuizId } from "../topics/topicsSlice";
 
 export const thunkActionCreator = (payload) => {
     return (dispatch) => {
-      // dispatch multiple actions here
-      debugger;
-      dispatch(addQuiz(payload));
-      dispatch(addQuizId(payload));
+        // dispatch multiple actions here
+        dispatch(quizzesSlice.actions.addQuiz(payload));
+    
+        dispatch(addQuizId(payload));
     };
 };
 
-const quizzesSlice = createSlice({
-    name: 'quizzes',
-    initialState: {
-        quizzes: {}
-    },
-    reducers: {
-        addQuiz: (state, action) => {
-            state.quizzes[action.payload.id] = action.payload
+export const quizzesSlice = createSlice(
+    {
+        name: 'quizzes',
+        initialState: {
+            quizzes: {}
+        },
+        reducers: {
+            addQuiz: (state, action) => {
+                debugger;
+                const { name, topicId, id } = action.payload;
+                state.quizzes[id] = {
+                    name: name,
+                    id: id,
+                    topicId: topicId,
+                    cardIds: []
+                    
+                }
+            }
         }
-    }
-})
+    })
 
-export const {addQuiz} = quizzesSlice.actions;
-
+export const { addQuiz } = quizzesSlice.actions;
 export const selectQuiz = (state) => state.quizzes.quizzes;
-
 export default quizzesSlice.reducer;
